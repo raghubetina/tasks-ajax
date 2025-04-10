1485ed3e753d1962a0ba22e923837e58b0665ec8
Raghu Betina <raghu@firstdraft.com>
2025-04-10 00:38:50 -0500

    Add Stimulus controller to fadeIn

---
+++ app/javascript/controllers/fade_controller.js
@@ -0,0 +1,8 @@
+// app/javascript/controllers/fade_controller.js
+import { Controller } from "@hotwired/stimulus";
+export default class extends Controller {
+  connect() {
+    // Hide and fade in the element (uses jQuery, which is already available)
+    $(this.element).hide().fadeIn("fast");
+  }
+}
