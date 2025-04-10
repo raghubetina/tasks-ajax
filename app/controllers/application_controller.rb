4bdb851b637591623895b6c847c0d627d6b432cb
Raghu Betina <raghu@firstdraft.com>
2025-04-10 00:31:50 -0500

    Add random ID to cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :set_random_id
+
+  def set_random_id
+    cookies[:random_id] ||= rand(100000)
+  end
 end
