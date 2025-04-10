19f8e746de259ef665b26ec3961a761c77ca131c
Raghu Betina <raghu@firstdraft.com>
2025-04-10 00:46:45 -0500

    fadeIn only for new tasks

--- app/javascript/controllers/fade_controller.js
+++ app/javascript/controllers/fade_controller.js
@@ -3,6 +3,6 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   connect() {
     // Hide and fade in the element (uses jQuery, which is already available)
-    $(this.element).hide().fadeIn("fast");
+    $(this.element).hide().fadeIn(2000);
   }
 }
