b067caab849ccf4eb6798b8fa95fd1b944d6c68f
Raghu Betina <raghu@firstdraft.com>
2025-04-12 14:13:04 -0700

    Real-time updates

--- app/models/task.rb
+++ app/models/task.rb
@@ -10,4 +10,9 @@
 #
 class Task < ApplicationRecord
   validates :content, presence: true
+
+  # Individual callbacks for more control
+  after_create_commit -> { broadcast_prepend_to "tasks", partial: "tasks/task", locals: { task: self, fade_in: true } }
+  after_update_commit -> { broadcast_replace_to "tasks" }
+  after_destroy_commit -> { broadcast_replace_to "tasks", partial: "tasks/task", locals: { task: self, fade_out: true } }
 end
